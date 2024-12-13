f6d3df9c666761b9755d5498698eda94cbf4b23a
GuillaumeSOULICHANH <g.soulichanh@gmail.com>
2024-12-13 16:28:30 +0100

    post

---
+++ src/Controller/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+// include "data/posts.php";
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/post", name="app_post")
+     */
+    public function index(): Response
+    {
+        include(__DIR__.'/../../data/posts.php');
+        $posts= json_decode($posts);
+        dump($posts);
+
+        // return $this->render('post/index.html.twig', [
+        //     'controller_name' => 'PostController',
+        // ]);
+        return $this->render('post/post.html.twig', [
+            'posts' => $posts,
+            'tags' => $tags,
+        ]);
+    }
+}
