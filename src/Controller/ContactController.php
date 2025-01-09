3d0cf3820a733ea30563065f1e80d882f5fa4df5
GuillaumeSOULICHANH <g.soulichanh@gmail.com>
2025-01-09 16:24:28 +0100

    formulaire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,14 +14,26 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="app_contact")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(
+            ContactFormType::class
+        );
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+            dd($data);
+        }
+
         // return $this->json([
         //     'message' => 'Welcome to your new controller!',
         //     'path' => 'src/Controller/ContactController.php',
         // ]);
         return $this->render('contact/index.html.twig',[
-            'firstname' => 'Guillaume'
+            'firstname' => 'Guillaume',
+            'form'=> $form->createView()
         ]);
     }
 }
